6981367671607c94852beaa9a8efd79b33f651e9
Carlos Egea <charlie.egea@gmail.com>
2024-10-13 22:34:00 +0200

    Add scheduler based on Arduino::Ticker

---
+++ remote_homeassistant/include/scheduler.h
@@ -0,0 +1,12 @@
+#ifndef scheduler_h
+#define scheduler_h
+
+#include <Ticker.h>
+
+/**
+ * @brief Set the up scheduler
+ *  
+ */
+void setup_scheduler() ;
+
+#endif 
---
+++ remote_homeassistant/src/scheduler.cpp
@@ -0,0 +1,12 @@
+#include "scheduler.h"
+#include "provisioning.h"
+#include "mqtt_client.h"
+
+Ticker ticker_command_server;
+Ticker ticker_mqtt;
+
+void setup_scheduler() {
+  ticker_command_server.attach_ms(100, command_server_provisioning);
+
+  ticker_mqtt.attach(5, MQTT_publish_time_since_init);
+}
